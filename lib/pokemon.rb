564f47cfe26865de45e434c24e0573066f253c72
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-17 18:50:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,21 +1,26 @@
-class Pokemon
-  
-  attr_accessor :id, :name, :type, :db
-  
+attr_reader :id, :name, :type, :db
+
   def initialize (id:, name:, type:, db:)
     @id = id 
     @name = name 
-    @type = type
+    @type = type 
     @db = db
   end
-  
-  def self.save 
+
+  def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO songs (name, album)
+      INSERT INTO pokemon (name, type)
       VALUES (?, ?)
     SQL
-    
-    
+
+    id = db.execute(sql,[name, type])
+    self.new(id: id, name: name, type: type, db: db)
   end
-    
-end
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    name = pokemon[1]
+    type = pokemon[2]
+
+
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: db)
+  end
